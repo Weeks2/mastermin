468440e51f4be149f6591c05d6881792aef7b9db
Christian Ibarra <ch.ibarrac@gmail.com>
2024-05-07 07:42:57 -0600

    init

--- c11_game/main.c
+++ c11_game/main.c
@@ -5,45 +5,6 @@
 #include <ctype.h>
 #include "game.h";
 
-char combinacion[TAM_COMBINACION + 1];
-char history[MAX_HISTORIAL][TAM_COMBINACION + 1];
-int num_jugadas = 1;
-Jugador jugadores[MAX_JUGADORES];
-int num_jugadores = 0;
-
-void generarCombinacion(char combinacion[]) {
-    srand(time(NULL));
-    for (int i = 0; i < TAM_COMBINACION; i++) {
-        combinacion[i] = colores[rand() % NUM_COLORES];
-    }
-    combinacion[TAM_COMBINACION] = '\0'; // Terminador de cadena
-}
-
-void mostrarHistorial(void) {
-    printf("\nHistorial de jugadas:\n");
-    for (int i = 0; i < num_jugadas; i++) {
-        printf("%s\n", history[i]);
-    }
-}
-
-void agregar_jugador() {
-    printf("---- > NUEVO JUGADOR \n");
-    if (num_jugadores < MAX_JUGADORES) {
-        printf("Nombre: ");
-        fgets(jugadores[num_jugadores].nombre, MAX_NOMBRE, stdin);
-        jugadores[num_jugadores].nombre[strcspn(jugadores[num_jugadores].nombre, "\n")] = '\0';
-        printf("Intentos permitidos: ");
-        scanf("%d", &jugadores[num_jugadores].intentos);
-        num_jugadores++;
-
-        while (getchar() != '\n') {
-        }
-    } else {
-        printf("Ya se ha alcanzado el máximo de jugadores.\n");
-    }
-    mostrarJugadores(jugadores,num_jugadores);
-}
-
 void play(void) {
     agregar_jugador();
     int intentos = 3;
